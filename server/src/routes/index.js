44cff2f5fffd3c2b344d2da0db3962ff0d99edfa
Ali Moteirek <ali.moteirek@yh.nackademin.se>
2023-01-12 15:56:52 +0100

    Person Route

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -4,6 +4,7 @@ import userRoute from './userRoute.js';
 const router = express.Router();
 
 router.use('/user', userRoute);
+router.use('/person', personRoute);
 router.use('/:mediaType', mediaRoute);
 
 export default router;
