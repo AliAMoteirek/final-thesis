eaf6b5d02ed5d8eb439f9ebda085e6b711e89cf1
Ali Moteirek <ali.moteirek@yh.nackademin.se>
2023-01-13 12:19:03 +0100

    Added theme to app component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,29 @@
+import { ThemeProvider } from '@mui/material/styles';
+import { useSelector } from 'react-redux';
+import themeConfigs from './configs/themeConfigs';
+import { ToastContainer } from 'react-toastify';
+import CssBaseline from '@mui/material/CssBaseline';
+
 const App = () => {
-  return <div className="App">App</div>;
+  const { themeMode } = useSelector((state) => state.themeMode);
+  return (
+    <ThemeProvider theme={themeConfigs.custom({ mode: themeMode })}>
+      {/* Configure toastify */}
+      <ToastContainer
+        position="bottom-left"
+        autoClose={50000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        pauseOnFocusLoss
+        pauseOnHover
+        theme={themeMode}
+      />
+
+      {/* Mui reset css */}
+      <CssBaseline />
+    </ThemeProvider>
+  );
 };
 
 export default App;
