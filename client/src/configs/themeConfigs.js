6683d231842325e9b193b57d0308fe3969307bab
Ali Moteirek <ali.moteirek@yh.nackademin.se>
2023-01-13 13:35:53 +0100

    Created footer component

--- client/src/configs/themeConfigs.js
+++ client/src/configs/themeConfigs.js
@@ -11,7 +11,8 @@ const themeConfigs = {
       mode === themeModes.dark
         ? {
             primary: {
-              main: '#24de72ff',
+              // main: '#24de72ff',
+              main: '#5ac5d2ff',
               contrastText: '#ffffff',
             },
             secondary: {
@@ -19,13 +20,16 @@ const themeConfigs = {
               contrastText: '#ffffff',
             },
             background: {
-              default: '#191d24',
-              paper: '#404040',
+              // default: '#191d24',
+              default: '#0a2029ff',
+              // paper: '#404040',
+              paper: '#1d333cff',
             },
           }
         : {
             primary: {
-              main: '#24de72ff',
+              // main: '#24de72ff',
+              main: '#5ac5d2ff',
             },
             secondary: {
               main: '#7bceb9',
