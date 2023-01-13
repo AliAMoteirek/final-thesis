e2780533c225be15464ee88f42217c3b145b3914
Ali Moteirek <ali.moteirek@yh.nackademin.se>
2023-01-13 12:51:06 +0100

    Added routes to App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,10 @@ import { useSelector } from 'react-redux';
 import themeConfigs from './configs/themeConfigs';
 import { ToastContainer } from 'react-toastify';
 import CssBaseline from '@mui/material/CssBaseline';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import MainLayout from './components/layout/MainLayout';
+import PageWrapper from './components/common/PageWrapper';
+import routes from './routes/routes';
 
 const App = () => {
   const { themeMode } = useSelector((state) => state.themeMode);
@@ -22,6 +26,46 @@ const App = () => {
 
       {/* Mui reset css */}
       <CssBaseline />
+
+      {/* app routes */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            {routes.map((route, index) =>
+              route.index ? (
+                <Route
+                  index
+                  key={index}
+                  element={
+                    route.state ? (
+                      <PageWrapper state={route.state}>
+                        {route.element}
+                      </PageWrapper>
+                    ) : (
+                      route.element
+                    )
+                  }
+                />
+              ) : (
+                <Route
+                  path={route.path}
+                  key={index}
+                  element={
+                    route.state ? (
+                      <PageWrapper state={route.state}>
+                        {route.element}
+                      </PageWrapper>
+                    ) : (
+                      route.element
+                    )
+                  }
+                />
+              )
+            )}
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      {/* app routes */}
     </ThemeProvider>
   );
 };
