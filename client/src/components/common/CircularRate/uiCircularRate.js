2d4674b3f9c87de4095588e94f963cb6a53a35e5
Ali Moteirek <ali.moteirek@yh.nackademin.se>
2023-01-14 11:26:56 +0100

    CircularRate component

---
+++ client/src/components/common/CircularRate/uiCircularRate.js
@@ -0,0 +1,19 @@
+const uiCircularRate = {
+  mainBox: {
+    position: 'relative',
+    display: 'inline-block',
+    width: 'max-content',
+  },
+  innerBox: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+};
+
+export default uiCircularRate;
