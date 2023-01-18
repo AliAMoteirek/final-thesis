fae5799842e997ad3b0f00ae8f5732a93bd9f31f
Ali Moteirek <ali.moteirek@yh.nackademin.se>
2023-01-18 12:32:56 +0100

    Added review to MovieDetail

--- client/src/components/common/MediaReviews/MediaReviews.jsx
+++ client/src/components/common/MediaReviews/MediaReviews.jsx
@@ -1,20 +1,10 @@
-import {
-  Box,
-  Button,
-  Divider,
-  Stack,
-  TextField,
-  Typography,
-} from '@mui/material';
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { toast } from 'react-toastify';
 import reviewApi from '../../../api/modules/reviewApi';
 import Container from '../Container';
-import TextAvatar from '../TextAvatar/TextAvatar';
-import ReviewItem from './ReviewItem';
-import SendOutlinedIcon from '@mui/icons-material/SendOutlined';
-import { LoadingButton } from '@mui/lab';
+import FilteredReviews from './FilteredReviews';
+import UserReview from './UserReview';
 
 const MediaReviews = ({ reviews, media, mediaType }) => {
   const { user } = useSelector((state) => state.user);
@@ -84,53 +74,19 @@ const MediaReviews = ({ reviews, media, mediaType }) => {
   return (
     <>
       <Container header={`Reviews (${reviewCount})`}>
-        <Stack spacing={4} marginBottom={2}>
-          {filteredReviews.map((item) => (
-            <Box key={item.id}>
-              <ReviewItem review={item} onRemoved={onRemoved} />
-              <Divider
-                sx={{
-                  display: { xs: 'block', md: 'none' },
-                }}
-              />
-            </Box>
-          ))}
-          {filteredReviews.length < listReviews.length && (
-            <Button onClick={onLoadMore}>load more</Button>
-          )}
-        </Stack>
-        {user && (
-          <>
-            <Divider />
-            <Stack direction="row" spacing={2}>
-              <TextAvatar text={user.displayName} />
-              <Stack spacing={2} flexGrow={1}>
-                <Typography variant="h6" fontWeight="700">
-                  {user.displayName}
-                </Typography>
-                <TextField
-                  value={content}
-                  onChange={(e) => setContent(e.target.value)}
-                  multiline
-                  rows={4}
-                  placeholder="Write your review"
-                  variant="outlined"
-                />
-                <LoadingButton
-                  variant="contained"
-                  size="large"
-                  sx={{ width: 'max-content' }}
-                  startIcon={<SendOutlinedIcon />}
-                  loadingPosition="start"
-                  loading={onRequest}
-                  onClick={onAddReview}
-                >
-                  post
-                </LoadingButton>
-              </Stack>
-            </Stack>
-          </>
-        )}
+        <FilteredReviews
+          filteredReviews={filteredReviews}
+          listReviews={listReviews}
+          onLoadMore={onLoadMore}
+          onRemoved={onRemoved}
+        />
+        <UserReview
+          content={content}
+          onAddReview={onAddReview}
+          onRequest={onRequest}
+          setContent={setContent}
+          user={user}
+        />
       </Container>
     </>
   );
